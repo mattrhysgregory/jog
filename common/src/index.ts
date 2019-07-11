e35c8dfdab32e1b556d93660e1703ef253295b42
mattrhysgregory <matt@mgregory.uk>
2019-07-11 19:55:53 +0100

    Got workspaces and gitignore stuff working

---
+++ common/src/index.ts
@@ -0,0 +1,5 @@
+export const ROOT_CONF = {
+  WS_URL: "ws://localhost:8080"
+};
+
+export * from "./types";
---
+++ common/src/types/msg.types.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+var RetroMessageTypes;
+(function (RetroMessageTypes) {
+    RetroMessageTypes["GOOD"] = "good";
+    RetroMessageTypes["BETTER"] = "better";
+    RetroMessageTypes["BAD"] = "bad";
+})(RetroMessageTypes = exports.RetroMessageTypes || (exports.RetroMessageTypes = {}));
